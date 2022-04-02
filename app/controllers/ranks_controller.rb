3c060efbffc20b14cbcc13157edccc749ecef2bc
keima-d08 <doikeima@gmail.com>
2022-04-03 01:06:17 +0900

    ランキングページの実装

---
+++ app/controllers/ranks_controller.rb
@@ -0,0 +1,6 @@
+class RanksController < ApplicationController
+
+  def index
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
  end
 
   resources :users, only: :show
+  resources :ranks, only: :index
 end
