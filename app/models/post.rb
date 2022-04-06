ad3a119c891cd6f0c654c03c26525e0fe7a0ad80
keima-d08 <100249962+keima-d08@users.noreply.github.com>
2022-04-07 02:13:02 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -25,6 +25,13 @@ class Post < ApplicationRecord
     end
   end
 
+  scope :search, -> (search_param = nil) {
+    return if search_param.blank?
+    joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = posts.id AND action_text_rich_texts.record_type = 'Post'")
+    .where("action_text_rich_texts.body LIKE ? OR posts.title LIKE ? ", "%#{search_param}%", "%#{search_param}%")
+  }
+end
+
   def favorited?(user)
     favorites.where(user_id: user.id).exists?
   end
