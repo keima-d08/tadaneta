20c404ff36c4125033a8ec8204c66976739a09c8
keima-d08 <100249962+keima-d08@users.noreply.github.com>
2022-04-08 13:12:18 +0900

    いいね一覧機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = @user.follower_user
   end
+
+  def favorites
+    @user = User.find(params[:id])
+    
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
+    @posts = Post.find(favorites)
+  end
+
 end
