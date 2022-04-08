12911f1004844ae3fdb18dac3f2e60f09f86d813
keima-d08 <100249962+keima-d08@users.noreply.github.com>
2022-04-08 19:02:22 +0900

    フォーム編集

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,9 +2,10 @@ class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'TVアニメ' },
-    { id: 3, name: '映画、OVA' },
-    { id: 4, name: '漫画' },
-    { id: 5, name: '小説' }
+    { id: 3, name: '映画' },
+    { id: 4, name: 'OVA' },
+    { id: 5, name: '漫画' },
+    { id: 6, name: '小説' }
   ]
 
   include ActiveHash::Associations
--- app/models/type.rb
+++ app/models/type.rb
@@ -4,7 +4,9 @@ class Type < ActiveHash::Base
     { id: 2, name: '解説' },
     { id: 3, name: '考察' },
     { id: 4, name: '感想' },
-    { id: 5, name: 'その他' }
+    { id: 5, name: 'レビュー' },
+    { id: 6, name: '雑学' },
+    { id: 7, name: 'その他' }
   ]
 
   include ActiveHash::Associations
