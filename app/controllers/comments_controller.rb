030d0525a8a7fcab1a2ba115c1a0fda2b560912c
keima-d08 <100249962+keima-d08@users.noreply.github.com>
2022-05-01 23:33:40 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to post_path(@comment.post)
+       redirect_to post_path(@comment.post)
     else
       redirect_back fallback_location: @post
     end
